c5bbcd983bf9a8fe9283bae6e36a2d13495d5d0d
Kevin-Kiprotich <kevinkiprotich0089@gmail.com>
2024-05-29 11:23:12 +0300

    Added logout sequence

--- users/urls.py
+++ users/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('login/',views.LoginView.as_view(),name='login'),
     path('signup/',views.SignUpView.as_view(),name='signup'),
     path('passwordupdate/',views.UpdatePassword.as_view()),
+    path('logout/',views.LogoutView.as_view()),
     path('resetpassword/',views.changePassword.as_view()),
     path('activate/<uidb64>/<token>', views.activate, name='activate'),
     path('update/<uidb64>/<token>', views.update, name='update'),
